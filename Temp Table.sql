7d04a2826003848b379e72acb256080301fdb3fc
SanimYousuf <sanimyousuf02@gmail.com>
2024-11-26 00:35:48 +0600

    Add additional info

--- Temp Table.sql
+++ Temp Table.sql
@@ -8,3 +8,21 @@ Salary int
 --Insert data from other table:
 Insert Into #tempTable Select
 * From [SQL Tutorial]..EmployeeSalary
+
+--Another example:
+CREATE TABLE #tempEmployee (
+JobTitle varchar(50),
+EmployeePerJob int,
+AvgAge int,
+AvgSalary int
+)
+
+--Insert New data from another table:
+INSERT INTO #tempEmployee 
+Select JobTitle, COUNT(JobTitle), AVG(Age), AVG(Salary) From
+[SQL Tutorial].dbo.EmployeeDemo Join [SQL Tutorial].dbo.EmployeeSalary
+ON EmployeeDemo.EmployeeID = EmployeeSalary.EmployeeID
+Group by JobTitle
+
+--To create temp table again:
+DROP TABLE IF EXISTS #tempTable
