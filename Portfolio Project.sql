c882c84a90af5becf442f9314b06ab55539f1128
SanimYousuf <sanimyousuf02@gmail.com>
2024-12-01 15:50:29 +0600

    first commit

---
+++ Portfolio Project.sql
@@ -0,0 +1,6 @@
+-- Select Data we are going to use in this project:
+Select location, date, total_cases, new_cases, total_deaths, population
+From CovidDeaths
+Order By 1,2
+
+-- Total cases VS Total deaths:
